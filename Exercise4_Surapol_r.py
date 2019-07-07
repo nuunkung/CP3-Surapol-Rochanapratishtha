b672c44d3c556bf21f5853e91bfe5eb4a1b41b7d
Nuunkung <52436899+nuunkung@users.noreply.github.com>
2019-07-07 13:51:26 +0700

    Add files via upload

---
+++ Exercise4_Surapol_r.py
@@ -0,0 +1,9 @@
+Eng_score = 80.00
+GB_score = 78.00
+ICS_score = 99.00
+CP_score = 90.00
+print("--- Your Score ---")
+print("Foudation English :", Eng_score)
+print("General Business :", GB_score)
+print("Introduction to Computer Systems :",ICS_score)
+print("Computer Programming :",CP_score)
